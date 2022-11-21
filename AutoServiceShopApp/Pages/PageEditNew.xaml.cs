a3164f2d8d216b974350bdb9f54cdcf9327ca658
KAB6-10 <KAB6-10@DESKTOP-CBIUQLJ>
2022-11-21 12:35:27 +0300

    Добавьте файлы проекта.

---
+++ AutoServiceShopApp/Pages/PageEditNew.xaml.cs
@@ -0,0 +1,34 @@
+﻿using AutoServiceShopApp.AppDataFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoServiceShopApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageEditNew.xaml
+    /// </summary>
+    public partial class PageEditNew : Page
+    {
+        public PageEditNew(Product product)
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
