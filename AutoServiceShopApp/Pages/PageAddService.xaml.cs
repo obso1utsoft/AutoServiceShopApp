fa1cf6eb00c528c58a8957eef5740862035a0354
obso1utsoft <oleg.ledenev.2018@bk.ru>
2022-12-05 12:21:05 +0400

    Update

--- AutoServiceShopApp/Pages/PageAddService.xaml.cs
+++ AutoServiceShopApp/Pages/PageAddService.xaml.cs
@@ -33,7 +33,7 @@ namespace AutoServiceShopApp.Pages
             {
                 if (TxtDiscount.Text == "")
                 {
-                    TxtDiscount.Text = null;
+                    TxtDiscount.Text = "0";
                 };
                 Service service = new Service()
                 {
--- AutoServiceShopApp/Pages/PageProduct.xaml.cs
+++ AutoServiceShopApp/Pages/PageProduct.xaml.cs
@@ -35,15 +35,22 @@ namespace AutoServiceShopApp.Pages
             timer.Start();
         }
 
+        /// <summary>
+        /// Обновление данных в реальном времени и логика поиска
+        /// </summary>
         public void UpdateData(object sender, object e)
         {
             var HistoryProduct = ConnectOdb.conObj.Product.ToList();
             ListProduct.ItemsSource = HistoryProduct;
+
             ListProduct.ItemsSource = ConnectOdb.conObj.Product.Where(x => x.Title.ToLower().Contains(TxtSearch.Text) |
             x.Description.ToLower().Contains(TxtSearch.Text) |
             x.Manufacturer.Name.ToLower().Contains(TxtSearch.Text)).ToList();
         }
 
+        /// <summary>
+        /// Логика работы скролла при наведении на товар
+        /// </summary>
         private void ListBox_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
         {
             e.Handled = true;
@@ -54,18 +61,44 @@ namespace AutoServiceShopApp.Pages
             parent.RaiseEvent(eventArg);
         }
 
+        /// <summary>
+        /// Переключения на страницу PageEditNew.xaml
+        /// </summary>
         private void BtnEdit_Click(object sender, RoutedEventArgs e)
         {
             FrameObj.frameMain.Navigate(new PageEditNew((sender as Button).DataContext as Product));
         }
 
+        /// <summary>
+        /// Переключения на страницу PageSaleHistory.xaml
+        /// </summary>
         private void BtnSalehistory_Click(object sender, RoutedEventArgs e)
         {
             FrameObj.frameMain.Navigate(new PageSaleHistory((sender as Button).DataContext as Product));
         }
+
+        /// <summary>
+        /// Переключения на страницу PageAddProduct.xaml
+        /// </summary>
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
             FrameObj.frameMain.Navigate(new PageAddProduct());
+        }
+
+        /// <summary>
+        /// Логика печати
+        /// </summary>
+        private void BtnPrint_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.PrintDialog Printdlg = new System.Windows.Controls.PrintDialog();
+            if ((bool)Printdlg.ShowDialog().GetValueOrDefault())
+            {
+                Size pageSize = new Size(Printdlg.PrintableAreaWidth, Printdlg.PrintableAreaHeight);
+                // sizing of the element.
+                gridPanelListProduct.Measure(pageSize);
+                gridPanelListProduct.Arrange(new Rect(5, 5, pageSize.Width, pageSize.Height));
+                Printdlg.PrintVisual(gridPanelListProduct, Title);
+            }
         }
     }
 }
